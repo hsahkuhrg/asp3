f515c8d9d1f36cd494d20cab370db526c1fdd633
Vitalik Vlasenko <vitaliy.vlasenko@cts.kh.ua>
2019-02-16 14:31:51 +0200

    asp3

---
+++ ASP10/DataWork/Models/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace DataWork.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Respons> Respons { get; set; }
+        public DbSet<Form> Forms { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Teg> Tegs { get; set; }
+    }
+}
